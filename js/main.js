47f09110d3762fe4cb9da087c37ec9ee33666ad4
ruuuulee <etily24@gmail.com>
2022-05-22 13:05:07 +0800

    add drag and delete

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-var container1 = document.querySelector(".container");
-var zIndex = 2;
-var colors = ["#344648", "#7d8e95", "#ffbb98"];
+let container1 = document.querySelector(".container");
+let zIndex = 2;
+let colors = ["#344648", "#7d8e95", "#ffbb98"];
 
 $(document).ready(function () {
   $(".button").on("click", function () {
@@ -16,54 +16,83 @@ function createDiv() {
   return document.createElement("div");
 }
 function getTop() {
-  var wHeight = window.innerHeight - 200;
+  let wHeight = window.innerHeight - 200;
   return Math.random() * wHeight + "px";
 }
 function getLeft() {
-  var wWidth = window.innerWidth - 200;
+  let wWidth = window.innerWidth - 200;
   return Math.random() * wWidth + "px";
 }
 
 function addNote() {
-  var note = createDiv();
+  let note = createDiv();
   note.classList.add("note");
   note.style.top = getTop();
   note.style.left = getLeft();
   note.style.zIndex = zIndex;
   zIndex++;
 
-  var noteText = createDiv();
+  let noteText = createDiv();
   noteText.innerText = $("#memo").val();
   noteText.classList.add("note-text");
   noteText.style.backgroundColor = colors[Math.floor(Math.random() * 3)];
+  noteText.addEventListener("mousedown", (e) => {
+    e.preventDefault();
+    let startX = 0;
+    let startY = 0;
+    let item = e.target.parentElement;
 
-  var color1 = createDiv();
+    document.onmousemove = function (e) {
+      startX = e.clientX - e.offsetX;
+      startY = e.clientY - e.offsetY;
+      x = startX + e.offsetX;
+      y = startY + e.offsetY;
+      item.style.left = x + "px";
+      item.style.top = y + "px";
+    };
+    document.onmouseup = function () {
+      document.onmousemove = null;
+      document.onmouseup = null;
+    };
+  });
+
+  let color1 = createDiv();
   color1.classList.add("colorBox");
   color1.classList.add("color1");
   color1.addEventListener("click", (e) => {
-    var item = e.target.parentElement.children[0];
+    let item = e.target.parentElement.children[0];
     item.style.backgroundColor = colors[0];
   });
 
-  var color2 = createDiv();
+  let color2 = createDiv();
   color2.classList.add("colorBox");
   color2.classList.add("color2");
   color2.addEventListener("click", (e) => {
-    var item = e.target.parentElement.children[0];
+    let item = e.target.parentElement.children[0];
     item.style.backgroundColor = colors[1];
   });
 
-  var color3 = createDiv();
+  let color3 = createDiv();
   color3.classList.add("colorBox");
   color3.classList.add("color3");
   color3.addEventListener("click", (e) => {
-    var item = e.target.parentElement.children[0];
+    let item = e.target.parentElement.children[0];
     item.style.backgroundColor = colors[2];
+  });
+
+  let deleteBtn = createDiv();
+  deleteBtn.classList.add("colorBox");
+  deleteBtn.classList.add("deleteBtn");
+  deleteBtn.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
+  deleteBtn.addEventListener("click", (e) => {
+    let item = e.target.parentElement.parentElement;
+    item.remove();
   });
 
   note.appendChild(noteText);
   note.appendChild(color1);
   note.appendChild(color2);
   note.appendChild(color3);
+  note.appendChild(deleteBtn);
   container1.appendChild(note);
 }
